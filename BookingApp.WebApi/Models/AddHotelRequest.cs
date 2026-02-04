829f707afb2ead2cf13fddeb7efb1d004d68b496
selinozluk <selinozluk36@gmail.com>
2026-02-04 21:10:02 +0300

    feat: add validation rules for hotel creation request

---
+++ BookingApp.WebApi/Models/AddHotelRequest.cs
@@ -0,0 +1,18 @@
+﻿using BookingApp.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookingApp.WebApi.Models
+{
+    public class AddHotelRequest
+    {
+        [Required]
+        public string Name { get; set; }
+        public int? Stars { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public AccomodationType AccomodationType { get; set; }
+
+        public List<int> FeatureIds { get; set; }
+    }
+}
