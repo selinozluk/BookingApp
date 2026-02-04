92dfc34a635d356c16a6ba2bb0c63557b3682251
selinozluk <selinozluk36@gmail.com>
2026-02-04 20:32:31 +0300

    feat: add feature service interface

---
+++ BookingApp.Business/Operations/Feature/IFeatureService.cs
@@ -0,0 +1,15 @@
+﻿using BookingApp.Business.Operations.Feature.Dtos;
+using BookingApp.Business.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Business.Operations.Feature
+{
+    public interface IFeatureService
+    {
+        Task<ServiceMessage> AddFeature(AddFeatureDto feature);
+    }
+}
