f755cdb2b238605fbb6e81bb616aa892b5840c4a
selinozluk <selinozluk36@gmail.com>
2026-02-04 20:38:45 +0300

    feat: add system settings entity for maintenance mode

---
+++ BookingApp.Data/Entities/SettingEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Data.Entities
+{
+    public class SettingEntity : BaseEntity
+    {
+        public bool MaintenanceMode { get; set; }
+    }
+}
