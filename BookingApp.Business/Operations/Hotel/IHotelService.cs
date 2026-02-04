6414c3b05866f322b0659c7f5e8fec63bf639998
selinozluk <selinozluk36@gmail.com>
2026-02-04 20:45:24 +0300

    feat: add hotel service interface

---
+++ BookingApp.Business/Operations/Hotel/IHotelService.cs
@@ -0,0 +1,20 @@
+﻿using BookingApp.Business.Operations.Hotel.Dtos;
+using BookingApp.Business.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Business.Operations.Hotel
+{
+    public interface IHotelService
+    {
+        Task<ServiceMessage> AddHotel(AddHotelDto hotel);
+        Task<HotelDto> GetHotel(int id);
+        Task<List<HotelDto>> GetHotels();
+        Task<ServiceMessage> AdjustHotelStars(int id, int changeTo);
+        Task<ServiceMessage> DeleteHotel(int id);
+        Task<ServiceMessage> UpdateHotel(UpdateHotelDto hotel);
+    }
+}
