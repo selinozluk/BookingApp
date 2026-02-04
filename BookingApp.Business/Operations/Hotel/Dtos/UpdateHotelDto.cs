2b6c43f8640c3d696a9cfb9fb799af947111c69e
selinozluk <selinozluk36@gmail.com>
2026-02-04 20:43:20 +0300

    feat: add DTO for hotel update

---
+++ BookingApp.Business/Operations/Hotel/Dtos/UpdateHotelDto.cs
@@ -0,0 +1,20 @@
+﻿using BookingApp.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Business.Operations.Hotel.Dtos
+{
+    public class UpdateHotelDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Stars { get; set; }
+        public string Location { get; set; }
+        public AccomodationType AccomodationType { get; set; }
+        public List<int> FeatureIds { get; set; }
+    }
+}
