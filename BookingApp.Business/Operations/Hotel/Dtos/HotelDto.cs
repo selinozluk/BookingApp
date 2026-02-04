9197de1773b7184203eee3c0755c9bafda637763
selinozluk <selinozluk36@gmail.com>
2026-02-04 20:41:22 +0300

    feat: add hotel response DTO

---
+++ BookingApp.Business/Operations/Hotel/Dtos/HotelDto.cs
@@ -0,0 +1,20 @@
+﻿using BookingApp.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Business.Operations.Hotel.Dtos
+{
+    public class HotelDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Stars { get; set; }
+        public string Location { get; set; }
+        public AccomodationType AccomodationType { get; set; }
+        public List<HotelFeatureDto> Features { get; set; }
+
+    }
+}
