17fa28821afea2e283f837c8b1630f2eacb42460
selinozluk <selinozluk36@gmail.com>
2026-02-04 20:34:47 +0300

    feat: implement maintenance settings service

---
+++ BookingApp.Business/Operations/Setting/SettingManager.cs
@@ -0,0 +1,48 @@
+﻿using BookingApp.Data.Entities;
+using BookingApp.Data.Repositories;
+using BookingApp.Data.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Business.Operations.Setting
+{
+    public class SettingManager : ISettingService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<SettingEntity> _settingRepository;
+
+        public SettingManager(IUnitOfWork unitOfWork, IRepository<SettingEntity> settingRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _settingRepository = settingRepository;
+        }
+
+        public bool GetMaintenanceState()
+        {
+            var maintenanceState = _settingRepository.GetById(1).MaintenanceMode;
+
+            return maintenanceState;
+        }
+
+        public async Task ToggleMaintenance()
+        {
+            var setting = _settingRepository.GetById(1);
+            setting.MaintenanceMode = !setting.MaintenanceMode;
+            _settingRepository.Update(setting);
+
+            try
+            {
+                await _unitOfWork.SaveChangesAsync();
+
+            }
+            catch (Exception)
+             {
+
+                throw new Exception("Bakım durumu güncellenirken bir hata ile karşılaşıldı.");
+             }
+        }
+    }
+}
